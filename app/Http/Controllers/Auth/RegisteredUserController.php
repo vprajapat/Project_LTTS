d6bb34f369263babfc74e936ea30e5d6180079a7
Vinod Prajapat <67252591+vprajapat@users.noreply.github.com>
2022-02-18 09:04:38 +0530

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        $user->attachRole('user');
+        $user->attachRole('admin');
         event(new Registered($user));
 
         Auth::login($user);
